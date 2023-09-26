25890bb1f726fcb2445cbda82659af89dab66ff0
Mike Henke <henke.mike@gmail.com>
2023-09-25 21:44:03 -0500

    removing secret key

--- dogapi/settings.py
+++ dogapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t6^-n)*z&@ob%y3v0^f%nj3j5-!us72bt--a(*g(125&=i#p2k'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
