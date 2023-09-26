406fabb063b5db897ee89eb62c129aabd83715f1
Mike Henke <henke.mike@gmail.com>
2023-09-25 21:44:33 -0500

    removing secret key

--- dogapi/settings.py
+++ dogapi/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+#import secret key from settings/local_settings.py
+from .local_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
